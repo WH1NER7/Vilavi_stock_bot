a51c701831eb11113f8e1f500ee6ad506e6e75d0
WH1NER7 <danil-2208@mail.ru>
2024-07-05 12:35:46 +0500

    first commit

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def fetch_and_save_report(cookies, session):
             for cell in col:
                 try:
                     if len(str(cell.value)) > max_length:
-                        max_length = len(str(cell.value)) / 2
+                        max_length = len(str(cell.value))
                 except:
                     pass
             adjusted_width = (max_length + 2)
